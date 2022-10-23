f09faf747aa55daad602713930e46f1242f79c35
cspitt99 <104328339+cspitt99@users.noreply.github.com>
2022-10-23 14:13:43 -0400

    deleted a lot of materials as the one call api does not work anymore

--- script.js
+++ script.js
@@ -1,35 +1,36 @@
 //define all html static selectors
-var cityEl= document.querySelector("#city")
-var cityFormEl= document.querySelector("#city-form")
-var cityHeaderEl= document.querySelector("#city-header")
-var tempEl= document.querySelector("#temp")
-var windEl= document.querySelector("#wind")
-var humidityEl= document.querySelector("#humidity")
-var uviEl= document.querySelector("#uvi")
+var cityEl = document.querySelector("#city")
+var cityFormEl = document.querySelector("#city-form")
+var cityHeaderEl = document.querySelector("#city-header")
+var tempEl = document.querySelector("#temp")
+var windEl = document.querySelector("#wind")
+var humidityEl = document.querySelector("#humidity")
 
-var dayEl= document.querySelector("#day")
-var tempDayEl= document.querySelector("#temp-day")
-var windDayEl= document.querySelector("#wind-day")
-var humidityDayEl= document.querySelector("#humidity-day")
+var api = "1b653f94bddf254b414b6bf02b4be622"
 
-var api="1b653f94bddf254b414b6bf02b4be622"
+var currentInput = localStorage.getItem("city");
 
 
 //definition of functions
 
-function displayWeather(event){
-    event.preventDefeault()
-    var cityName=cityEl.value
-    var urlCurrent=`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${api}&units=imperial`
+function displayWeather(event) {
+    var cityName = cityEl.value
+    var urlCurrent = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${api}&units=imperial`
 
     fetch(urlCurrent)
-    .then(function(response){
-        return response.json()
-    })
-    .then(function(currentDatea){
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function (currentData) {
 
-        console.log(currentDatea)
-    })
+            console.log(currentData)
+            cityHeaderEl.textContent = currentData.name
+            tempEl.textContent = currentData.main.temp
+            windEl.textContent = currentData.wind.speed
+            humidityEl.textContent = currentData.main.humidity
+
+        })
+
 }
 
 
